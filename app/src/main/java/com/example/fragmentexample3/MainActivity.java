7ff54f7c9ae70af4e7bb01d90337799e96fa6c68
ToloTom <tmargaryan@oxy.edu>
2021-02-25 13:34:09 -0800

    First Commit

--- app/src/main/java/com/example/fragmentexample3/MainActivity.java
+++ app/src/main/java/com/example/fragmentexample3/MainActivity.java
@@ -9,15 +9,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean twoPane = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // if we cannot find the second fragment in the main layout
+        // that means we are in portrait
+
+        if(findViewById(R.id.fragContainerView_land_second) != null){
+            twoPane = true;
+        }
+
+        // loading in 1 or 2 fragments based on the layout
+        if(!twoPane){ // port
+            loadFragment(new FirstFragment(), R.id.fragContainerView_first);
+            Button button = findViewById(R.id.button_Personality);
+            Button button1 = findViewById(R.id.button_HouseInfo);
+            button.setOnClickListener(v -> launchActivity(v));
+            button1.setOnClickListener(v -> launchActivity1(v));
+        }
+
+        else{
+            loadFragment(new FirstFragment(), R.id.fragContainerView_land_first);
+            loadFragment(new SecondFragment(), R.id.fragContainerView_land_second);
+            loadFragment(new ThirdFragment(), R.id.fragContainerView_land_third);
+        }
 
     }
 
@@ -32,6 +54,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void launchActivity(View view){
         Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+
+    public void launchActivity1(View view){
+        Intent intent = new Intent(this, ThirdActivity.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/fragmentexample3/ThirdActivity.java
+++ app/src/main/java/com/example/fragmentexample3/ThirdActivity.java
@@ -2,27 +2,23 @@ package com.example.fragmentexample3;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-public class SecondActivity extends AppCompatActivity {
+public class ThirdActivity extends AppCompatActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
-
-        // load fragment two into second activity
-        loadFragment(new SecondFragment(), R.id.fragContainer_second);
-
+        setContentView(R.layout.activity_third);
+        loadFragment(new ThirdFragment(), R.id.fragContainer_third);
     }
 
     public void loadFragment(Fragment fragment, int id){
         FragmentManager fragmentManager = getSupportFragmentManager();
-        // create a fragment transaction to begin the transaction and replace the fragment
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        //replacing the placeholder - fragmentContainterView with the fragment that is passed as parameter
         fragmentTransaction.replace(id, fragment);
         fragmentTransaction.commit();
     }
